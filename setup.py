e88873bb4820260c156cf8d854880dc2eb11726a
David Carpenter <carpenterd777@gmail.com>
2021-10-20 22:24:21 -0400

    rename to cpa again

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ Setup module to make CLI extensible.
 from setuptools import setup
 
 setup(
-    name='commit-prefix-analysis',
+    name='cpa',
     version='1.0.0',
-    py_modules=['commit-prefix-analysis.py'],
+    py_modules=['cpa.py'],
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'commit-prefix-analysis = commit-prefix-analysis:cli',
+            'cpa = cpa:cli',
         ],
     },
 )
