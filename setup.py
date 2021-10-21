9ed9de0399dc85304dd0531c38392a3a8215ebac
David Carpenter <carpenterd777@gmail.com>
2021-10-20 22:15:23 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+Setup module to make CLI extensible.
+"""
+
+from setuptools import setup
+
+setup(
+    name='cpa',
+    version='1.0.0',
+    py_modules=['cpa.py'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cpa = cpa:cli',
+        ],
+    },
+)
